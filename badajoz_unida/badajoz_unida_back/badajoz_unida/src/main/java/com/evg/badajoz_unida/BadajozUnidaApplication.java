c8d195f357177114a783bc6eb8c44d9880c16327
Daniel García <daniel.garcia@preving.com>
2023-04-29 17:09:53 +0200

    Inicialización de front y back

---
+++ badajoz_unida/badajoz_unida_back/badajoz_unida/src/main/java/com/evg/badajoz_unida/BadajozUnidaApplication.java
@@ -0,0 +1,13 @@
+package com.evg.badajoz_unida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadajozUnidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadajozUnidaApplication.class, args);
+	}
+
+}
