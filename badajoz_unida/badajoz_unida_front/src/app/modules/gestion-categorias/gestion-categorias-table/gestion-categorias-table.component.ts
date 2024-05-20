326d5df712c014a6c15616639ee17a185f9b7ae7
nacedodev <121324366+nacedodev@users.noreply.github.com>
2024-05-20 18:40:33 +0200

    UPDATE: correccion de dropdowns de la seccion de admin

--- badajoz_unida/badajoz_unida_front/src/app/modules/gestion-categorias/gestion-categorias-table/gestion-categorias-table.component.ts
+++ badajoz_unida/badajoz_unida_front/src/app/modules/gestion-categorias/gestion-categorias-table/gestion-categorias-table.component.ts
@@ -17,7 +17,7 @@ import Swal from "sweetalert2";
 import { DataTableDirective } from "angular-datatables";
 import { FormBuilder, FormGroup } from "@angular/forms";
 import { LocalizedComponent } from "src/app/config/localize.component";
-
+import '@popperjs/core';
 @Component({
   selector: "app-gestion-categorias-table",
   templateUrl: "./gestion-categorias-table.component.html",
